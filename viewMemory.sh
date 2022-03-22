17e10b77768f257e001eae45d0d43116966e57c7
Hwang-97 <qqq4472@gmail.com>
2022-03-22 23:15:59 +0900

    Add viewMemory.sh var_1.1

---
+++ viewMemory.sh
@@ -0,0 +1,14 @@
+
+hn=`hostname -f`
+
+while [ 1 ] 
+do
+	echo "============ START ============"
+	echo ""
+	echo ""
+	echo sudo | free -m
+	echo ""
+	echo "============= END ============="
+        echo ""
+	sleep $1
+done	
