f4bb998eeccbdd38f12aee85b654253fed8eb469
Hwang-97 <qqq4472@gmail.com>
2022-03-23 22:17:44 +0900

    Add memory view

--- viewMemory.sh
+++ viewMemory.sh
@@ -1,14 +1,18 @@
 
 hn=`hostname`
-
+#total        used        free      shared  buff/cache   available
 while [ 1 ] 
 do
 	echo "============ START ============"
 	echo "[ $hn ]"
 	echo ""
 	echo sudo | free -m | awk '{if($1=="Mem:" || $1=="total") print $0}'
-	echo ""
+	totalMemory=`echo sudo | free -m | awk '{if($1=="Mem:") print $2}'`
+	usedMemory=`echo sudo | free -m | awk '{if($1=="Mem:") print $3}'`
+	echo "totalMemory = $totalMemory"
+	echo "usedMemory = $usedMemory"
+	echo "userd % : $totalMemory/$usedMemory"#숫자로 변경해야 됨
 	echo "============= END ============="
-        echo ""
+    echo ""
 	sleep $1
 done	
