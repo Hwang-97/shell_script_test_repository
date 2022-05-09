bc3155c73695c82d4b20f27a21f219f0191e649c
qqq4472 <qqq4472@gmail.com>
2022-05-09 12:56:25 +0000

    write c code

--- autoGitCommit.sh
+++ autoGitCommit.sh
@@ -4,19 +4,31 @@
 path="/home/ubuntu/test_git/C_Study/oracle_linux_server/"
 today=$(date "+%Y%m%d")
 fileName="auto_commit_${today}.c"
+txt="
 
+#include<stdio.h>
+
+void main(){
+	printf(\"   $today auto commit!!   \");
+}
+
+"
 cmd="touch $path$fileName"
 
 $cmd
 
 if [ -f $path$fileName ]; then
 
+	echo "$txt" > $path$fileName
+	gcc $path$fileName -o "${path}auto_commit_${today}"
+        
 	cd "/home/ubuntu/test_git/C_Study/"
 	git add *
 	git commit -m "$today auto commit"
 	git push
-
-	echo "sucsses"
+	
+	chmod 777 $path$fileName
+	echo "`${path}./auto_commit_${today}` >>> sucsses"
 
 else
 
